b77a8cf6d054b166e3b56746d111eac7612a24c7
Hunter Gayden <gayden.hunter@gmail.com>
2020-08-13 17:07:16 -0400

    Accept multiple guesses, exit on correct guess

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,30 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is {}", secret_number);
 
-    println!("Enter your guess: ");
+    loop {
+        println!("Enter your guess: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    //  trim() must be used to remove the newline character inserted by
-    //  pressing the 'enter' key
-    //  expect is necessary to handle any other parsing errors
-    let guess: u32 = guess.trim().parse().expect("Please type a valid number!");
+        //  trim() must be used to remove the newline character inserted by
+        //  pressing the 'enter' key
+        //  expect is necessary to handle any other parsing errors
+        let guess: u32 = guess.trim().parse().expect("Please type a valid number!");
 
-    // println!();
-    println!("You guessed {}", guess);
+        // println!();
+        println!("You guessed {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too low!"),
-        Ordering::Greater => println!("Too high!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
