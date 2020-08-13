5ff396a5c62e0890993f97df532626a6557f2a42
Hunter Gayden <gayden.hunter@gmail.com>
2020-08-13 17:03:54 -0400

    Compare input to "secret" number

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -15,6 +16,17 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    //  trim() must be used to remove the newline character inserted by
+    //  pressing the 'enter' key
+    //  expect is necessary to handle any other parsing errors
+    let guess: u32 = guess.trim().parse().expect("Please type a valid number!");
+
     // println!();
     println!("You guessed {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too low!"),
+        Ordering::Greater => println!("Too high!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
